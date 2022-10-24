a26141a07b390680914b1af5c80bf03e26c6d265
nithish <nithish98raja@gmail.com>
2022-10-24 10:51:03 +0530

    bug fixes

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -79,8 +79,8 @@ window.addEventListener('load', function(e){
     if(inputIndex >= dfa[dfaIndex]["input"].length){
       inputIndex = 0;
     }
-    refreshInput();
     inputPointer = -1;
+    refreshInput();
     refreshCanvas();
   });
 
